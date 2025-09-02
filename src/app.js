b018c365d5b9c5e2661f15bd97c4c83584f314bb
Cali2016-Pad <iztla1996@gmail.com>
2025-09-02 15:32:58 +0000

    final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Lilies', 'Roses', 'Tulips', 'Peonies', 'Daisies'];
+  let what = ['stems', 'petals', 'roots', 'seeds'];
+  let action = ['glisten', 'scream', 'cry', 'dance'];
+  let when = ['at night', 'during the afternoon', 'in the morning', 'during the witching hour'];
+
+  function getRandomNumber(a, b) {
+    return Math.floor(Math.random() * (b - a) + a);
+  }
+
+  function getRandom(anyArray) {
+    let max = anyArray.length;
+    let min = 0;
+    let random = getRandomNumber(min, max);
+    return anyArray[random];
+  }
+
+  function generateExcuse() {
+    return `${getRandom(who)} ${getRandom(what)} ${getRandom(action)} ${getRandom(when)}`;
+  }
+
+  document.querySelector('#excuse').innerText = generateExcuse();
+
+ 
+  document.querySelector("button").onclick = function() {
+  document.getElementById("excuse").innerText = generateExcuse();
+  };
 };
