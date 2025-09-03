0fd0ffdb17671086f0798e2f345c230688d2d120
Cali2016-Pad <iztla1996@gmail.com>
2025-09-03 02:16:27 +0000

    Cleaner

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
+
 import "bootstrap";
 import "./style.css";
-
 
 window.onload = function() {
   let who = ['Lilies', 'Roses', 'Tulips', 'Peonies', 'Daisies'];
@@ -8,25 +8,25 @@ window.onload = function() {
   let action = ['glisten', 'scream', 'cry', 'dance'];
   let when = ['at night', 'during the afternoon', 'in the morning', 'during the witching hour'];
 
-  function getRandomNumber(a, b) {
-    return Math.floor(Math.random() * (b - a) + a);
-  }
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
 
-  function getRandom(anyArray) {
-    let max = anyArray.length;
-    let min = 0;
-    let random = getRandomNumber(min, max);
-    return anyArray[random];
-  }
+function getRandom(anyArray) {
+  let max = anyArray.length;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
 
-  function generateExcuse() {
-    return `${getRandom(who)} ${getRandom(what)} ${getRandom(action)} ${getRandom(when)}`;
-  }
+function generateExcuse() {
+  return `${getRandom(who)} ${getRandom(what)} ${getRandom(action)} ${getRandom(when)}`;
+}
 
-  document.querySelector('#excuse').innerText = generateExcuse();
-
+document.querySelector('#excuse').innerText = generateExcuse();
  
-  document.querySelector("button").onclick = function() {
-  document.getElementById("excuse").innerText = generateExcuse();
-  };
+document.querySelector("button").onclick = function() {
+document.getElementById("excuse").innerText = generateExcuse();
+};
+
 };
